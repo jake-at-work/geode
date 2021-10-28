c6a631a5a495d779c9c4ebbd45fdff3b2c04cc55
Jacob Barrett <jbarrett@pivotal.io>
2021-10-28 13:04:40 -0700

    Version for 2.0.0

--- geode-serialization/src/main/java/org/apache/geode/internal/serialization/KnownVersion.java
+++ geode-serialization/src/main/java/org/apache/geode/internal/serialization/KnownVersion.java
@@ -209,6 +209,13 @@ public class KnownVersion extends AbstractVersion {
       new KnownVersion("GEODE", "1.15.0", (byte) 1, (byte) 15, (byte) 0, (byte) 0,
           GEODE_1_15_0_ORDINAL, true);
 
+  private static final short GEODE_2_0_0_ORDINAL = 500;
+
+  @Immutable
+  public static final KnownVersion GEODE_2_0_0 =
+      new KnownVersion("GEODE", "2.0.0", (byte) 2, (byte) 0, (byte) 0, (byte) 0,
+          GEODE_2_0_0_ORDINAL, true);
+
   /* NOTE: when adding a new version bump the ordinal by 10. Ordinals can be short ints */
 
   /**
@@ -222,7 +229,7 @@ public class KnownVersion extends AbstractVersion {
    * HIGHEST_VERSION when changing CURRENT !!!
    */
   @Immutable
-  public static final KnownVersion CURRENT = GEODE_1_15_0;
+  public static final KnownVersion CURRENT = GEODE_2_0_0;
 
   /**
    * A lot of versioning code needs access to the current version's ordinal
